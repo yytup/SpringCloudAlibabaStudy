2f480e13b91650fa995cd5533c0f0bceaca29602
yyt <876456826@qq.com>
2022-08-27 20:49:07 +0800

    20220827 搭建HystrixDashBoard并简单测试使用

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/yyt/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.yyt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: yyt
+ * @Data: 2022/8/27 20:35
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
